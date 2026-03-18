06cc50a2da06e5387516203b1dccd682b7964629
yatinn888 <youremail@example.com>
2026-03-18 15:47:14 +0530

    UC4: Display Hello with multiple names

--- src/main/java/org/example/helloapp.java
+++ src/main/java/org/example/helloapp.java
@@ -2,7 +2,20 @@ package org.example;
 
 public class helloapp {
     public static void main(String[] args) {
-        System.out.println("He" +
-                "llo World");
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            System.out.print("Hello ");
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+
+
+                if (i != args.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+        }
     }
 }
